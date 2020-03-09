a51710ef60c377291dd66c9663939c8466ac556c
indunil lakshitha <skgindunil@gmail.com>
2020-03-10 00:00:30 +0530

    initial 2

---
+++ crud/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column(TypeName ="nvarchar(250)")]
+        [Required]
+
+        public String FullName { get; set; }
+        [Column(TypeName = "varchar(100)")]
+
+        public String EmpCode { get; set; }
+        [Column(TypeName = "varchar(100)")]
+
+        public String Position { get; set; }
+        [Column(TypeName = "varchar(100)")]
+
+        public String OfficeLocation { get; set; }
+       
+
+    }
+}
